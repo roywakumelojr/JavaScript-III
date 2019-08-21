870607b001e361150173c40ec756c4c03b1251f9
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-08-21 19:04:30 -0400

    MVP Reached

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,16 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+};
+
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`;
+};
+
 
 /*
   === CharacterStats ===
@@ -22,6 +32,17 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(CharacterStatsAttributes) {
+  this.healthPoints = CharacterStatsAttributes.healthPoints;
+  this.name = CharacterStatsAttributes.name;
+  GameObject.call(this, CharacterStatsAttributes);
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage`;
+}
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,16 +53,27 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+function Humanoid(humanoidAttributes) {
+  this.team = humanoidAttributes.team;
+  this.weapons = humanoidAttributes.weapons;
+  this.language = humanoidAttributes.language;
+  CharacterStats.call(this, humanoidAttributes);
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +134,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,60 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: Refers to the function that is globally available on the window or console scope 
+
+* 2. Implicit Binding: Refers to the this statement being bound to the content of whatever is listed on the left side of the dot.
+
+* 3. New Binding: Refers to a situation where a object is created and returned by the constructor or builder function.
+
+* 4. Explicit Binding: Refers to when the 'this' function is explicitly defined to perform a specified action. 
 *
+
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+function sayName(name) {
+    console.log(this);
+    return name;
+}
+sayName("Roy");
+
 
 // Principle 2
-
 // code example for Implicit Binding
+const myObject = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+        console.log (`${this.greeting} my name is ${name}`);
+    }
+};
+myObject.sayHello('Roy');
+
 
 // Principle 3
-
 // code example for New Binding
+function Person(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.speak = function () {
+        return `Hello, my name is ${this.name}, and I am ${this.age} years old`
+    }
+}
+const roy = new Person({
+    name:'Roy',
+    age: 28,
+})
+const bonnie = new Person({
+    name:'Bonnie',
+    age: 9,
+})
+console.log (roy.speak());
+console.log (bonnie.speak());
+
 
 // Principle 4
-
 // code example for Explicit Binding
+console.log(roy.speak.call(bonnie));
+console.log(bonnie.speak.apply(roy));
